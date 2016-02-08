7d8618820c94eb29a8a65a1fb814d021f9522831
David L <davidylu@cmu.edu>
2016-02-07 20:26:29 -0800

    Start modeling gameworld: fruit and its derivatives.

---
+++ app/src/main/java/com/davidylu/games/bubbleapples/gameworld/Apple.java
@@ -0,0 +1,12 @@
+package com.davidylu.games.bubbleapples.gameworld;
+
+/**
+ * Class to represent an Apple.
+ *
+ * @author David Lu
+ * @version 02.07.16
+ */
+public class Apple extends Fruit
+{
+
+}
---
+++ app/src/main/java/com/davidylu/games/bubbleapples/gameworld/Fruit.java
@@ -0,0 +1,12 @@
+package com.davidylu.games.bubbleapples.gameworld;
+
+/**
+ * Fruit class for all floating fruits
+ *
+ * @author David Lu
+ * @version 02.07.16
+ */
+public abstract class Fruit
+{
+
+}
